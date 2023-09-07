9106dcb33ee3fcaa20c9f09a855cd6037611697c
Shivansh Agrawal <shivansh@akto.io>
2023-09-08 00:25:25 +0530

    testing auto deploy

--- .github/scripts/deploy-script.sh
+++ .github/scripts/deploy-script.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 echo "enter" >> file1.txt
-cd akto/infra
+cd ~/akto/infra
 docker-compose -f docker-compose-dashboard.yml pull
 docker-compose -f docker-compose-dashboard.yml down
 docker-compose -f docker-compose-dashboard.yml up -d
