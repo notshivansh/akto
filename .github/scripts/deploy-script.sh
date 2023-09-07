4d97115b5feb9562ec63b110c620fa6a616d6ce1
Shivansh Agrawal <shivansh@akto.io>
2023-09-07 22:13:56 +0530

    testing auto deploy

--- .github/scripts/deploy-script.sh
+++ .github/scripts/deploy-script.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
+echo "enter" >> file1.txt
 cd akto/infra
 docker-compose -f docker-compose-dashboard.yml pull
 docker-compose -f docker-compose-dashboard.yml down
 docker-compose -f docker-compose-dashboard.yml up -d
+echo "exit" >> file1.txt
--- .github/scripts/echo-script.sh
+++ .github/scripts/echo-script.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-echo "this is running an echo script"
+echo "this is running an echo script" >> file.txt
